e1d30c83c94136c259a3ff0fa03508dac2d8d3da
IrinaDemina <demina.irinka@gmail.com>
2018-05-15 22:35:40 +0500

    новая версия

--- homework_3.2.py
+++ homework_3.2.py
@@ -1,17 +1,8 @@
 import requests
-# AUTH_URL = "https://oauth.vk.com/authorize"
-# APP_ID = 6475174
-# auth_data = {
-#     "client_id": APP_ID,
-#     "display": "page",
-#     "scope": "friends",
-#     "response_type": "token",
-#     "v": "5.74"
-# }
 
 
 def find_common_friends(first, second):
-    TOKEN = "c91670ecbbfea58b50966022cafd08e03deacbe80f1a05a3d1e65e925995c616c3db5c108d2968d6d38b1"
+    TOKEN = "0e3f6e0f8a84edefa32612b259b0072823692821427d3ad51a6ec6a16b55d274a3cf640a056bbc763ff26"
     response = requests.get(
         "https://api.vk.com/method/friends.getMutual",
         params=dict(
@@ -21,15 +12,13 @@ def find_common_friends(first, second):
             target_uid=second
         )
     )
-    result = response.json().get("response", [])
-    return result
+    return response.json().get("response", [])
 
 
 def main():
     VK_URL = "https://vk.com/id"
     first = input("Введите ID первого друга: ")
     second = input("Введите ID второго друга: ")
-    find_common_friends(first, second)
     result_list = find_common_friends(first, second)
 
     for id in result_list:
